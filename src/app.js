79025c3dcec0ba5f5bb3b8395c4031e9fc8b0758
lucasinui <101011043+lucasinui@users.noreply.github.com>
2022-03-12 15:43:05 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { v4: uuid, validate: isUuid } = require('uuid');
 
@@ -11,23 +12,82 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  project = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(project);
+
+  return response.json(project);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs} = request.body;
+  
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if (projectIndex < 0 ) {
+    return response.json({error : 'Project not found.'})
+  }
+
+  // sem as chaves para const, é forçar algo a algum valor. 
+  
+  const project = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[projectIndex].likes,
+  };
+
+  repositories[projectIndex] = project;
+
+  return response.json(project);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if ( projectIndex < 0 ) {
+    return response.status(400).json({error: 'Project not found.'})
+  }
+
+  repositories.splice(projectIndex,1);
+
+  return response.status(200).json({message: 'Project deleted.'});
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const projectIndex = repositories.findIndex(project => project.id == id);
+
+  if (projectIndex < 0 ){
+    return response.status(400).json({ error: 'Project not found.'})
+  }
+  
+  repositories[projectIndex].likes++;
+  
+  return response.json(project);
+
 });
 
 module.exports = app;
+
+app.listen(3333);
+console.log('Back-end started');
